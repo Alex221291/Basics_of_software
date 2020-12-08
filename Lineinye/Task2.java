27189d59d380ae40e99f45059515ae576e91c6c2
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2020-12-08 21:37:39 +0300

    Задачи_Basics_of_software

---
+++ Lineinye/Task2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+//2. Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println(" Enter equals a b c :");
+        double a = console.nextDouble();
+        double b = console.nextDouble();
+        double c = console.nextDouble();
+        double z2 = (b + Math.sqrt(b * b + 4 * a * c)) / (2 * a) - Math.pow(a , 3) + Math.pow(b , -2);
+        System.out.println("z2 = " + z2);
+    }
+}
