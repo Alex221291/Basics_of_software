716674846d1217a497be72a53684f47f5b1a5c65
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2020-12-08 21:37:10 +0300

    Задачи_Basics_of_software

--- Lineinye/Task1.java
+++ Lineinye/Task1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 //1. Найдите значение функции: z = ( (a – 3 ) * b / 2) + c.
-public class LineTask1 {
+public class Task1 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println(" Enter equals a b c :");
