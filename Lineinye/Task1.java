afe8ff6b7546b7630c342715ad5882a3a2d23919
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2020-12-08 22:02:06 +0300

    Задачи_Basics_of_software

--- Lineinye/Task1.java
+++ Lineinye/Task1.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
+
 //1. Найдите значение функции: z = ( (a – 3 ) * b / 2) + c.
+
 public class Task1 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
