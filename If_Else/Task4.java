29bada9d18ba3f4d7c083129f8271101b5856fcf
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2020-12-09 21:48:19 +0300

    Задачи_Basics_of_software

---
+++ If_Else/Task4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//4. Заданы размеры А, В прямоугольного отверстия и размеры х, у, z кирпича.
+// Определить, пройдет ли кирпич через отверстие.
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println(" Enter A B :");
+        double A = console.nextDouble();
+        double B = console.nextDouble();
+        System.out.println(" Enter points x y z :");
+        double x = console.nextDouble();
+        double y = console.nextDouble();
+        double z = console.nextDouble();
+        if (((x <= A) && (y <= B))|| ((x <= B) && (y <= A))|| ((x <= A) && (z <= B))|| ((x <= B) && (z <= A))|| ((y <= A) && (z <= B))|| ((y <= B) && (z <= A))) {
+            System.out.println("the brick will pass through the hole");
+        }
+        else {
+            System.out.println("the brick won't pass through the hole");
+        }
+        }
+    }
