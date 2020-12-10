9583e77c878a95511463b777e4307a9cc4ccaf32
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2020-12-10 21:17:07 +0300

    Задачи_Basics_of_software

---
+++ Cycles/Task3.java
@@ -0,0 +1,12 @@
+//3. Найти сумму квадратов первых ста чисел.
+
+public class Task {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i < 101; i++) {
+            sum += i * i;
+        }
+        System.out.println("1^2 + 2^2 + 3^2 + .. + 99^2 + 100^2 = " + sum);
+
+    }
+}
