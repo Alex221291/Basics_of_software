f87d34deef701cd0a3e68bcc80341f8e80360a2d
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2020-12-10 21:58:37 +0300

    Задачи_Basics_of_software

--- Cycles/Task3.java
+++ Cycles/Task3.java
@@ -1,6 +1,6 @@
 //3. Найти сумму квадратов первых ста чисел.
 
-public class Task {
+public class Task3 {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 1; i < 101; i++) {
