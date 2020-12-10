2e18bb66fdd226f24a1c98a82fb7c16b7a96cd67
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2020-12-10 21:57:14 +0300

    Задачи_Basics_of_software

---
+++ Cycles/Task4.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+
+//4. Составить программу нахождения произведения квадратов первых двухсот чисел.
+
+public class Task {
+    public static void main(String[] args) {
+        //Scanner console = new Scanner(System.in);
+        BigInteger pr = new BigInteger("1");
+        for (int i = 1; i <= 200; i++) {
+            pr = pr.multiply(BigInteger.valueOf(i * i));
+        }
+        System.out.println("1^2 * 2^2 * 3^2 * .. * 199^2 * 200^2 = " + pr);
+
+    }
+}
